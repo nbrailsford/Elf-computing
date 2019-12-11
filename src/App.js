be96ef97f2550f56b817778497f987b271a27494
Nathan Brailsford <nbrailsford@msn.com>
2019-12-11 02:22:10 -0700

    'added imports'

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Customize from "./customize/Customize";
+import Header from "./Header/Header";
+import Cart from "./cart/Cart";
+import "./App.css";
 
-// Normalizes string as a slug - a string that is safe to use
-// in both URLs and html attributes
-import slugify from 'slugify';
-
-import './App.css';
-
-// This object will allow us to
-// easily convert numbers into US dollar values
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
+const USCurrencyFormat = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD"
 });
 
 class App extends Component {
   state = {
     selected: {
       Processor: {
-        name: '17th Generation Intel Core HB (7 Core with donut spare)',
+        name: "17th Generation Intel Core HB (7 Core with donut spare)",
         cost: 700
       },
-      'Operating System': {
-        name: 'Ubuntu Linux 16.04',
+      "Operating System": {
+        name: "Ubuntu Linux 16.04",
         cost: 200
       },
-      'Video Card': {
-        name: 'Toyota Corolla 1.5v',
+      "Video Card": {
+        name: "Toyota Corolla 1.5v",
         cost: 1150.98
       },
       Display: {
@@ -44,21 +40,18 @@ class App extends Component {
   };
 
   render() {
-
     return (
-      <div className="App">
-        <Header >
-        <main>
-          <Customize 
-               features={this.props.features} 
-               selected={this.state.selected}
-               updateFeature={this.updateFeature}  
-          >
-          <Cart 
-               selected={this.state.selected}    
-          >
-        </main>
-      </div>
+      <main>
+        <div className="App">
+          <Header />
+          <Customize
+            features={this.props.features}
+            selected={this.state.selected}
+            updateFeature={this.updateFeature}
+          />
+          <Cart selected={this.state.selected} />
+        </div>
+      </main>
     );
   }
 }

f917c7f116e375df946b21832286a04f2c81d412
Manish Poduval <manishpoduval@hotmail.com>
2019-12-11 07:47:00 +0530

    Updated App.js
    Session changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,66 @@
-import React from "react";
-import Header from "./Header/Header";
-import Customize from "./customize/Customize";
-import Cart from "./cart/Cart";
-import "./App.css";
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div className="content">
-        <Customize flexBasis="2" />
-        <Cart flexBasis="1" />
+// Normalizes string as a slug - a string that is safe to use
+// in both URLs and html attributes
+import slugify from 'slugify';
+
+import './App.css';
+
+// This object will allow us to
+// easily convert numbers into US dollar values
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD'
+});
+
+class App extends Component {
+  state = {
+    selected: {
+      Processor: {
+        name: '17th Generation Intel Core HB (7 Core with donut spare)',
+        cost: 700
+      },
+      'Operating System': {
+        name: 'Ubuntu Linux 16.04',
+        cost: 200
+      },
+      'Video Card': {
+        name: 'Toyota Corolla 1.5v',
+        cost: 1150.98
+      },
+      Display: {
+        name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+        cost: 1500
+      }
+    }
+  };
+
+  updateFeature = (feature, newValue) => {
+    const selected = Object.assign({}, this.state.selected);
+    selected[feature] = newValue;
+    this.setState({
+      selected
+    });
+  };
+
+  render() {
+
+    return (
+      <div className="App">
+        <Header >
+        <main>
+          <Customize 
+               features={this.props.features} 
+               selected={this.state.selected}
+               updateFeature={this.updateFeature}  
+          >
+          <Cart 
+               selected={this.state.selected}    
+          >
+        </main>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;

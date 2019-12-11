0810cebd7d3667aa0e9a7057c190643cad3cfc26
Nathan Brailsford <nbrailsford@msn.com>
2019-12-11 03:27:22 -0700

    got state to update and changed styles

--- src/cart/Cart.js
+++ src/cart/Cart.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import "./cart.css";
 
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
+const USCurrencyFormat = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD"
 });
 
 class Cart extends Component {
   render() {
     const summary = Object.keys(this.props.selected).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
+      const featureHash = feature + "-" + idx;
       const selectedOption = this.props.selected[feature];
 
       return (
@@ -27,17 +27,17 @@ class Cart extends Component {
       (acc, curr) => acc + this.props.selected[curr].cost,
       0
     );
-   
+
     return (
       <section className="main__summary">
-            <h2>Your cart</h2>
-            {summary}
-            <div className="summary__total">
-              <div className="summary__total__label">Total</div>
-              <div className="summary__total__value">
-                {USCurrencyFormat.format(total)}
-              </div>
-            </div>
+        <h2>Your cart</h2>
+        {summary}
+        <div className="summary__total">
+          <div className="summary__total__label">Total</div>
+          <div className="summary__total__value">
+            {USCurrencyFormat.format(total)}
+          </div>
+        </div>
       </section>
     );
   }
